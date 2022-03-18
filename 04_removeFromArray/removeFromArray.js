cc506feacd06b8653cfd5cab320dbbd0b0753362
Gustavo <gustavo.cordeiro.borges26@gmail.com>
2022-03-18 10:30:25 -0300

    Quarto exercício completo

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,8 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...remove) {
+    const filteredArray = array.filter(test => !remove.includes(test))
+    return filteredArray
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
