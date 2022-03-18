90903c13e67344f6c1158a2d6d83db84cb423273
Gustavo <gustavo.cordeiro.borges26@gmail.com>
2022-03-18 12:28:01 -0300

    Quinto exercício completo

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function(a,b) {
+    let sum = 0
+    if (b > a) {
+        for (i = a; i <= b; i++) {
+            sum += i
+        }
+    } else if (a > b) {
+        for (i = b; i <= a; i++) {
+            sum += i
+        }
+    }
 
+    if (sum < 0 || typeof a != 'number' || typeof b != 'number') {
+        return 'ERROR'
+    } else {
+        return sum
+    }
 };
 
 // Do not edit below this line
