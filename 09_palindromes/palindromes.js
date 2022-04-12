8c3d9ef2ac0037fc20baf967156310e763606652
Gustavo <gustavo.cordeiro.borges26@gmail.com>
2022-04-12 13:40:15 -0300

    Nono exercício completo

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let lower = string.toLowerCase().replace(/[^a-z]/g,"");
+    function reverseString() {
+        let splitString = lower.split("");
+        let reverseArray = splitString.reverse();
+        let reversedString = reverseArray.join("");
+        return reversedString;
+    };
+    return lower == reverseString() ? true : false;
 };
 
 // Do not edit below this line
